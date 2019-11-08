12ff2325eaba6505ad65e595eff13b94b551b423
Rich Sharp <richpsharp@gmail.com>
2019-11-08 10:36:00 -0800

    starting entry script

---
+++ nci_ndr_analysis.py
@@ -0,0 +1,13 @@
+"""Script to run option 1, solution 2."""
+LANDCOVER_URL = (
+    'https://storage.googleapis.com/critical-natural-capital-ecoshards/'
+    'Base_LULC_CR_updated1_md5_a63f1e8a0538e268c6ae8701ccf0291b.tif')
+
+
+def main():
+    """Entry point."""
+    pass
+
+
+if __name__ == '__main__':
+    main()
