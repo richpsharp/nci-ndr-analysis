6f12bf40117112a9774016e5f915a2ba869793e7
Rich Sharp <richpsharp@gmail.com>
2019-11-09 18:48:44 -0800

    making manager and worker files

---
+++ nci_ndr_manager.py
@@ -0,0 +1,87 @@
+"""NCI NDR Analysis.
+
+Design doc is available here:
+
+https://docs.google.com/document/d/
+1Iw8YxrXPSbSp5TemRo-mbfvxDiTpdCKqRrW1terp2gE/edit
+
+"""
+import argparse
+import datetime
+import logging
+import os
+import sys
+import zipfile
+
+import ecoshard
+import taskgraph
+
+WATERSHEDS_URL = (
+    'https://storage.googleapis.com/nci-ecoshards/'
+    'watersheds_globe_HydroSHEDS_15arcseconds_'
+    'blake2b_14ac9c77d2076d51b0258fd94d9378d4.zip')
+
+WORKSPACE_DIR = 'workspace_manager'
+ECOSHARD_DIR = os.path.join(WORKSPACE_DIR, 'ecoshards')
+CHURN_DIR = os.path.join(WORKSPACE_DIR, 'churn')
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=(
+        '%(asctime)s (%(relativeCreated)d) %(levelname)s %(name)s'
+        ' [%(funcName)s:%(lineno)d] %(message)s'),
+    stream=sys.stdout)
+LOGGER = logging.getLogger(__name__)
+
+
+def main(n_workers):
+    """Entry point."""
+    for dir_path in [WORKSPACE_DIR, ECOSHARD_DIR, CHURN_DIR]:
+        try:
+            os.makedirs(dir_path)
+        except OSError:
+            pass
+
+    task_graph = taskgraph.TaskGraph(WORKSPACE_DIR, n_workers)
+
+    # download watersheds
+    watersheds_zip_path = os.path.join(
+        ECOSHARD_DIR, os.path.basename(WATERSHEDS_URL))
+    LOGGER.debug(
+        'scheduing download of watersheds: %s', WATERSHEDS_URL)
+    watersheds_zip_fetch_task = task_graph.add_task(
+        func=ecoshard.download_url,
+        args=(WATERSHEDS_URL, watersheds_zip_path),
+        target_path_list=[watersheds_zip_path],
+        task_name='download watersheds zip')
+    watersheds_unzip_dir = watersheds_zip_path.replace('.zip', '')
+    unzip_token_path = os.path.join(
+        CHURN_DIR, '%s.UNZIPTOKEN' % os.path.basename(watersheds_unzip_dir))
+    LOGGER.debug(
+        'scheduing unzip of: %s', watersheds_zip_path)
+    watersheds_unzip_task = task_graph.add_task(
+        func=unzip_file,
+        args=(watersheds_zip_path, watersheds_unzip_dir),
+        target_path_list=[unzip_token_path],
+        dependent_task_list=[watersheds_zip_fetch_task],
+        task_name='unzip %s' % watersheds_zip_path)
+    task_graph.join()
+    task_graph.close()
+
+
+def unzip_file(zip_path, target_directory, token_file):
+    """Unzip contents of `zip_path` into `target_directory`."""
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(target_directory)
+    with open(token_file, 'w') as token_file:
+        token_file.write(str(datetime.datetime.now()))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='NCI NDR Analysis.')
+    parser.add_argument(
+        'n_workers', type=int, default=-1,
+        help='number of taskgraph workers to create')
+
+    args = parser.parse_args()
+    main(args.n_workers)
