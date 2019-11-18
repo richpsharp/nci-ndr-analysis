ba8ea6abeef486db682dc4727ef8fb4c445a1839
Rich Sharp <richpsharp@gmail.com>
2019-11-18 21:22:48 +0000

    had to pass the flags correctly

--- server_startup.sh
+++ server_startup.sh
@@ -9,4 +9,4 @@ mkdir /usr/local/workspace
 mount /dev/xvdb /usr/local/workspace
 echo "/dev/xvdb /usr/local/workspace ext4  defaults,nofail  0  2" >> /etc/fstab
 git clone https://github.com/richpsharp/nci-ndr-analysis.git /usr/local/workspace/nci-ndr-analysis
-docker run --rm -p 8080:8080 -v `pwd`:/var/workspace natcap/nci-ndr-execution:1 nci_ndr_watershed_manager.py 8080 10.0.1.57 > docker_log.txt
+docker run --rm -p 8080:8080 -v `pwd`:/var/workspace natcap/nci-ndr-execution:1 nci_ndr_manager.py --app_port 8080 --external_ip 10.0.1.57 > docker_log.txt
