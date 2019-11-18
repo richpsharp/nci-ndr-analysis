8e7a22860ef7dd71da903f88284f414670868061
Rich Sharp <richpsharp@gmail.com>
2019-11-18 21:42:24 +0000

    setting docker to start on boot

--- server_startup.sh
+++ server_startup.sh
@@ -2,6 +2,7 @@
 yum update -y
 amazon-linux-extras install docker -y
 service docker start
+systemctl enable docker
 usermod -a -G docker ec2-user
 yum install git -y
 mkfs -t ext4 /dev/xvdb
--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 yum update -y
 amazon-linux-extras install docker -y
 service docker start
+systemctl enable docker
 usermod -a -G docker ec2-user
 yum install git -y
 git clone https://github.com/richpsharp/nci-ndr-analysis.git /root/nci-ndr-analysis
