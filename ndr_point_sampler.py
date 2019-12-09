ab2068bd85425a9cb9cd431e289ee4dad3deb3be
Rich Sharp <richpsharp@gmail.com>
2019-12-09 11:27:35 -0800

    downloading watersheds too

--- ndr_point_sampler.py
+++ ndr_point_sampler.py
@@ -7,12 +7,18 @@ import ecoshard
 import taskgraph
 
 WORKSPACE_DIR = 'ndr_point_sampler_workspace'
-
+ECOSHARD_DIR = os.path.join(WORKSPACE_DIR, 'ecoshard')
 NDR_WATERSHED_DATABASE_URL = (
     'https://storage.googleapis.com/ipbes-ndr-ecoshard-data/'
     'ndr_global_run_database_md5_fa32958c7024e8e93d067ecfd0c4d419.sqlite3')
+WATERSHEDS_URL = (
+    'https://nci-ecoshards.s3-us-west-1.amazonaws.com/'
+    'watersheds_globe_HydroSHEDS_15arcseconds_'
+    'blake2b_14ac9c77d2076d51b0258fd94d9378d4.zip')
+
 NDR_WATERSHED_DATABASE_PATH = os.path.join(
-    WORKSPACE_DIR, 'ndr_global_run_database.sqlite3')
+    ECOSHARD_DIR, 'ndr_global_run_database.sqlite3')
+WATERSHEDS_DIR = os.path.join(ECOSHARD_DIR)
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -24,8 +30,21 @@ LOGGER = logging.getLogger(__name__)
 logging.getLogger('taskgraph').setLevel(logging.INFO)
 
 
+def download_and_unzip(url, target_dir, target_token_path):
+    """Download `url` to `target_dir` and touch `target_token_path`."""
+    zipfile_path = os.path.join(target_dir, os.path.basename(url))
+    LOGGER.debug('url %s, zipfile_path: %s', url, zipfile_path)
+    ecoshard.download_url(url, zipfile_path)
+
+    with zipfile.ZipFile(zipfile_path, 'r') as zip_ref:
+        zip_ref.extractall(target_dir)
+
+    with open(target_token_path, 'w') as touchfile:
+        touchfile.write(f'unzipped {zipfile_path}')
+
+
 if __name__ == '__main__':
-    for dir_path in [WORKSPACE_DIR]:
+    for dir_path in [WORKSPACE_DIR, ECOSHARD_DIR]:
         try:
             os.makedirs(dir_path)
         except OSError:
@@ -38,6 +57,14 @@ if __name__ == '__main__':
         args=(NDR_WATERSHED_DATABASE_URL, NDR_WATERSHED_DATABASE_PATH),
         target_path_list=[NDR_WATERSHED_DATABASE_PATH],
         task_name='download ndr watershed database')
+
+    watersheds_done_token_path = os.path.join(
+        WORKSPACE_DIR, 'watersheds.UNZIPPED')
+    download_watersheds_task = task_graph.add_task(
+        func=download_and_unzip,
+        args=(WATERSHEDS_URL, WATERSHEDS_DIR, watersheds_done_token_path),
+        target_path_list=[watersheds_done_token_path],
+        task_name='download and unzip watersheds')
 
     task_graph.join()
     task_graph.close()
