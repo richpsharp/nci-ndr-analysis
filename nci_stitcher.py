053697286a9af4c79d442c27b4eba2e4538b0623
Rich Sharp <richpsharp@gmail.com>
2020-01-28 16:32:29 -0800

    adding a buffer generator for ndr

--- nci_stitcher.py
+++ nci_stitcher.py
@@ -23,7 +23,7 @@ GLOBAL_NODATA_VAL = -1
 
 def make_empty_wgs84_raster(
         cell_size, nodata_value, target_datatype, target_raster_path,
-        target_token_complete_path):
+        target_token_complete_path, center_point=None, buffer_range=None):
     """Make a big empty raster in WGS84 projection.
 
     Parameters:
@@ -37,6 +37,10 @@ def make_empty_wgs84_raster(
         target_token_complete_path (str): this file is created if the
             mosaic to target is successful. Useful for taskgraph task
             scheduling.
+        center_point (tuple): if not None, this is the center point to start
+            an AOI around.
+        buffer_range (float): if `center_point` is not null contains the degree
+            buffer to build around `center_point` for a square AOI.
 
     Returns:
         None.
@@ -48,10 +52,16 @@ def make_empty_wgs84_raster(
     except OSError:
         pass
 
-    n_cols = int(360.0 / cell_size)
-    n_rows = int(180.0 / cell_size)
-
-    geotransform = (-180.0, cell_size, 0.0, 90.0, 0, -cell_size)
+    if not center_point:
+        n_cols = int(360.0 / cell_size)
+        n_rows = int(180.0 / cell_size)
+        geotransform = (-180.0, cell_size, 0.0, 90.0, 0, -cell_size)
+    else:
+        n_cols = int(buffer_range / cell_size)
+        n_rows = int(buffer_range / cell_size)
+        geotransform = (
+            center_point[0]-buffer_range, cell_size,
+            0.0, center_point[1]+buffer_range, 0, -cell_size)
 
     target_raster = gtiff_driver.Create(
         target_raster_path, n_cols, n_rows, 1, target_datatype,
@@ -81,8 +91,9 @@ if __name__ == '__main__':
         '[BASENAME]/workspace_worker/[BASENAME]_[FID]/intermediate_outputs/stream.tif',
         ]
     for raster_path_pattern in raster_path_base_list:
-        global_raster_path = os.path.join(WORKSPACE_DIR, os.path.basename(
-            raster_path_pattern))
+        global_raster_path = os.path.join(
+            WORKSPACE_DIR, '%s_stitch.tif' % os.path.basename(
+                raster_path_pattern))
         target_token_complete_path = '%s.INITALIZED' % os.path.splitext(
             global_raster_path)[0]
         task_graph.add_task(
@@ -90,6 +101,10 @@ if __name__ == '__main__':
             args=(
                 WGS84_CELL_SIZE, GLOBAL_NODATA_VAL, gdal.GDT_Float32,
                 global_raster_path, target_token_complete_path),
+            kwargs={
+                'center_point': (-117, 38),
+                'buffer_range': 1.0,
+            },
             target_path_list=[target_token_complete_path],
             task_name='make empty %s' % os.path.basename(raster_path_pattern))
 
