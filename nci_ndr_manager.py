053a2f5c0bc0239293bbb6862fd9919ec4b478a4
Rich Sharp <richpsharp@gmail.com>
2020-02-07 11:40:22 -0800

    better logging about whta's being removed

--- nci_ndr_manager.py
+++ nci_ndr_manager.py
@@ -635,6 +635,7 @@ def send_job(watershed_fid_tuple_list):
         LOGGER.exception(
             'something bad happened, on %s for %s',
             worker_ip_port, watershed_fid_tuple_list)
+        LOGGER.debug('removing %s from worker set', worker_ip_port)
         GLOBAL_WORKER_STATE_SET.remove_host(worker_ip_port)
         raise
     finally:
