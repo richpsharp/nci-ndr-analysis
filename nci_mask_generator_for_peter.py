ea8ffcf4f07e86be1f2b4c503e38832a4496ead9
Rich Sharp <richpsharp@gmail.com>
2020-04-27 19:38:53 -0700

    bigger

--- nci_mask_generator_for_peter.py
+++ nci_mask_generator_for_peter.py
@@ -355,7 +355,8 @@ def main():
     clipped_vrt_raster_path = os.path.join(CHURN_DIR, 'clipped_dem.tif')
     pygeoprocessing.warp_raster(
         dem_vrt_raster_path, vrt_info['pixel_size'],
-        clipped_vrt_raster_path, 'near', target_bb=[-122, 32, 124, 34])
+        clipped_vrt_raster_path, 'near', target_bb=[-122, 32, -130, 40],
+        nthreads=multiprocessing.cpu_count())
     dem_vrt_raster_path = clipped_vrt_raster_path
 
     # create meters to degree array
